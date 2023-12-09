0c63bf11fe7bce7d9e6c7578935f8ae46ade69af
Jose Daniel Oropeza Soria <danioropezasoria@gmail.com>
2023-12-09 10:29:52 -0400

    Added hero section and the initial animations for AboutMe page.

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -1,109 +1,138 @@
-import React, { useState, useEffect } from 'react';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
+import React from 'react';
 import styled from 'styled-components';
-import { navDelay, loaderDelay } from '@utils';
-import { usePrefersReducedMotion } from '@hooks';
+import { StaticImage } from 'gatsby-plugin-image';
 
 const StyledHeroSection = styled.section`
-  ${({ theme }) => theme.mixins.flexCenter};
-  flex-direction: column;
-  align-items: flex-start;
+${({ theme }) => theme.mixins.flexCenter};
+  padding: 0;
+  margin-top: -70px;
+  flex-direction: row;
+  justify-content: space-evenly;
   min-height: 100vh;
   height: 100vh;
-  padding: 0;
 
   @media (max-height: 700px) and (min-width: 700px), (max-width: 360px) {
     height: auto;
   }
 
-  h1 {
-    margin: 0 0 30px 4px;
-    color: var(--green);
-    font-family: var(--font-mono);
-    font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
-    font-weight: 400;
+  .hero-photo {
+    ${({ theme }) => theme.mixins.boxShadow};
+    display: block;
+    position: relative;
+    border-radius: var(--border-radius);
+    background-color: var(--green);
 
-    @media (max-width: 480px) {
-      margin: 0 0 20px 2px;
+    &:hover,
+    &:focus {
+      outline: 0;
+      transform: translate(-4px, -4px);
+
+      &:after {
+        transform: translate(8px, 8px);
+      }
+
+      .img {
+        filter: none;
+        mix-blend-mode: normal;
+      }
+    }
+
+    .img {
+      position: relative;
+      border-radius: var(--border-radius);
+      mix-blend-mode: multiply;
+      filter: grayscale(100%) contrast(1);
+      transition: var(--transition);
+    }
+
+    &:before,
+    &:after {
+      content: '';
+      display: block;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      border-radius: var(--border-radius);
+      transition: var(--transition);
+    }
+
+    &:before {
+      top: 0;
+      left: 0;
+      background-color: var(--navy);
+      mix-blend-mode: screen;
+    }
+
+    &:after {
+      border: 2px solid var(--green);
+      top: 14px;
+      left: 14px;
+      z-index: -1;
     }
   }
 
-  h3 {
-    margin-top: 5px;
-    color: var(--slate);
-    line-height: 0.9;
-  }
+  .hero-details {
+    h1 {
+      margin: 0 0 30px 4px;
+      color: var(--green);
+      font-family: var(--font-mono);
+      font-size: clamp(var(--fz-sm), 5vw, var(--fz-md));
+      font-weight: 400;
 
-  p {
-    margin: 20px 0 0;
-    max-width: 540px;
-  }
+      @media (max-width: 480px) {
+        margin: 0 0 20px 2px;
+      }
+    }
 
-  .email-link {
-    ${({ theme }) => theme.mixins.bigButton};
-    margin-top: 50px;
+    h3 {
+      margin-top: 5px;
+      color: var(--slate);
+      line-height: 0.9;
+      font-size: clamp(20px, 5vw, 50px);
+    }
+
+    p {
+      margin: 20px 0 0;
+      max-width: 540px;
+    }
   }
 `;
 
 const Hero = () => {
-  const [isMounted, setIsMounted] = useState(false);
-  const prefersReducedMotion = usePrefersReducedMotion();
-
-  useEffect(() => {
-    if (prefersReducedMotion) {
-      return;
-    }
-
-    const timeout = setTimeout(() => setIsMounted(true), navDelay);
-    return () => clearTimeout(timeout);
-  }, []);
-
-  const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Daniel Oropeza.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
-  const four = (
+  const Greeting = <h1>Hi, my name is</h1>;
+  const Name = <h2 className="big-heading">Daniel Oropeza</h2>;
+  const Profession = <h3 className="big-heading">Full-stack Sofware Engineer</h3>;
+  const Phrase = (
     <>
       <p>
-        I’m a software engineer specializing in building (and occasionally designing) exceptional
-        digital experiences. Currently, I’m focused on building accessible, human-centered products
-        at{' '}
-        <a href="https://upstatement.com/" target="_blank" rel="noreferrer">
-          Upstatement
-        </a>
-        .
+        Frase chingona Frase chingona Frase chingona Frase chingona Frase chingona Frase chingona Frase chingona
       </p>
     </>
   );
-  const five = (
-    <a
-      className="email-link"
-      href="https://www.newline.co/courses/build-a-spotify-connected-app"
-      target="_blank"
-      rel="noreferrer">
-      Check out my course!
-    </a>
-  );
 
-  const items = [one, two, three, four, five];
+  const heroItems = [Greeting, Name, Profession, Phrase];
 
   return (
     <StyledHeroSection>
-      {prefersReducedMotion ? (
-        <>
-          {items.map((item, i) => (
-            <div key={i}>{item}</div>
-          ))}
-        </>
-      ) : (
-        <TransitionGroup component={null}>
-          {isMounted &&
-            items.map((item, i) => (
-              <CSSTransition key={i} classNames="fadeup" timeout={loaderDelay}>
-                <div style={{ transitionDelay: `${i + 1}00ms` }}>{item}</div>
-              </CSSTransition>
-            ))}
-        </TransitionGroup>
-      )}
+      <div className="hero-photo">
+        <StaticImage
+          className="img"
+          src="../../images/me.jpg"
+          quality={100}
+          aspectRatio={1}
+          formats={['AUTO', 'WEBP', 'AVIF']}
+          alt="Daniel Oropeza"
+          placeholder="blurred"
+          layout="fixed"
+          width={325}
+          height={325}
+        />
+      </div>
+      <div className="hero-details">
+        {heroItems.map((item, i) => (
+          <div key={i}>{item}</div>
+        ))}
+      </div>
     </StyledHeroSection>
   );
 };
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,37 @@
-import React from 'react';
-import styled from 'styled-components';
-import { Hero, About, Jobs, Featured, Projects, Contact } from '@components';
+/* eslint-disable react/jsx-key */
+import React, { useEffect, useRef } from 'react';
+import { srConfig } from '@config';
+import scrollReveal from '@utils/scrollReveal';
+import { Hero } from '@components';
+import { usePrefersReducedMotion } from '@hooks';
 
-const StyledMainContainer = styled.main`
-  counter-reset: section;
-`;
+const AboutMePage = () => {
+  const prefersReducedMotion = usePrefersReducedMotion();
+  const revealSections = useRef([]);
 
-const AboutMePage = () => (
-  <>
-    <StyledMainContainer className="fillHeight">
-      <Hero />
-      <About />
-      <Jobs />
-      <Featured />
-      <Projects />
-      <Contact />
-    </StyledMainContainer>
-  </>
-);
+  useEffect(() => {
+    if (prefersReducedMotion) {
+      return;
+    }
+
+    revealSections.current.forEach((ref, i) => {
+      scrollReveal.reveal(ref, srConfig(i * 200));
+    });
+  }, []);
+
+  const sections = [
+    <Hero />,
+  ];
+
+  return (
+    <div className={prefersReducedMotion ? '' : 'load-hidden'}>
+      {sections.length > 0 && sections.map((section, i) => (
+        <div key={i} ref={el => (revealSections.current[i] = el)}>
+          {section}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default AboutMePage;
