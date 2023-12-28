f4d21f9e4541f2839a58e8c5746ba00f2069adac
Jose Daniel Oropeza Soria <danioropezasoria@gmail.com>
2023-12-28 19:56:16 -0400

    Update some changes related to gatsby-config

---
+++ README.md
@@ -0,0 +1,4 @@
+node 18.12.1 (Currently using 64-bit executable)
+
+npm --version
+8.19.2
--- gatsby-config.js
+++ gatsby-config.js
@@ -4,8 +4,8 @@ module.exports = {
   siteMetadata: {
     title: 'Daniel Oropeza',
     description:
-      'Daniel Oropeza is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'http://localhost', // No trailing slash allowed!
+      'Daniel Oropeza is a full-stack software engineer.',
+    siteUrl: 'https://danioropeza.com', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
     twitterUsername: '@danioropeza',
   },
