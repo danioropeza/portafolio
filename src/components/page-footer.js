7c5db2a8b03639dde0795ac7307723bad6e78d05
Jose Daniel Oropeza Soria <danioropezasoria@gmail.com>
2024-01-01 23:42:34 -0400

    Added styles for 404 page

--- src/components/page-footer.js
+++ src/components/page-footer.js
@@ -4,15 +4,16 @@ import styled from 'styled-components';
 const StyledFooter = styled.footer`
   ${({ theme }) => theme.mixins?.flexCenter};
   flex-direction: column;
+  padding-top: 70px;
   height: auto;
   text-align: center;
   color: var(--light-slate);
   font-family: var(--font-mono);
   line-height: 1;
   font-size: var(--fz-xxs);
-
+  
   @media (max-width: 1080px) {
-    padding-top: 20px;
+    padding-top: 40px;
     font-size: var(--fz-xxxs);
   }
 `;
--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link } from 'gatsby';
 import { Helmet } from 'react-helmet';
-import { CSSTransition, TransitionGroup } from 'react-transition-group';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
-import { navDelay } from '@utils';
-import { Layout } from '@components';
-import { usePrefersReducedMotion } from '@hooks';
 
 const StyledMainContainer = styled.main`
   ${({ theme }) => theme.mixins?.flexCenter};
@@ -27,18 +23,7 @@ const StyledHomeButton = styled(Link)`
   margin-top: 40px;
 `;
 
-const NotFoundPage = ({ location }) => {
-  const [isMounted, setIsMounted] = useState(false);
-  const prefersReducedMotion = usePrefersReducedMotion();
-
-  useEffect(() => {
-    if (prefersReducedMotion) {
-      return;
-    }
-
-    const timeout = setTimeout(() => setIsMounted(true), navDelay);
-    return () => clearTimeout(timeout);
-  }, []);
+const NotFoundPage = () => {
 
   const content = (
     <StyledMainContainer className="fillHeight">
@@ -49,21 +34,15 @@ const NotFoundPage = ({ location }) => {
   );
 
   return (
-    <Layout location={location}>
+    <>
       <Helmet title="Page Not Found" />
 
-      {prefersReducedMotion ? (
-        <>{content}</>
-      ) : (
-        <TransitionGroup component={null}>
-          {isMounted && (
-            <CSSTransition timeout={500} classNames="fadeup">
-              {content}
-            </CSSTransition>
-          )}
-        </TransitionGroup>
-      )}
-    </Layout>
+      <StyledMainContainer className="fillHeight">
+        <StyledTitle>404</StyledTitle>
+        <StyledSubtitle>Page Not Found</StyledSubtitle>
+        <StyledHomeButton to="/">About Me</StyledHomeButton>
+      </StyledMainContainer>
+    </>
   );
 };
 
